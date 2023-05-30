e5df3701ca1752feaaa7f82fb1ce43bd5559db44
azero-dev <fr.rodmar@gmail.com>
2023-05-30 23:24:10 +0100

    fix error package json

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useMemo } from "react";
+import { useRef, useState, useMemo, useCallback } from "react";
 import { searchMovies } from "../services/movies";
 
 export function useMovies({ search, sort }) {
@@ -7,8 +7,7 @@ export function useMovies({ search, sort }) {
   const [error, setError] = useState(null);
   const previousSearch = useRef(search);
 
-  const getMovies = useMemo(() => {
-    return async ({ search }) => {
+  const getMovies = useCallback(async ({ search }) => {
       if (search === previousSearch.current) return;
 
       try {
@@ -22,8 +21,7 @@ export function useMovies({ search, sort }) {
       } finally {
         setLoading(false);
       }
-    };
-  }, []);
+}, []);
 
   const sortedMovies = useMemo(() => {
     return sort
